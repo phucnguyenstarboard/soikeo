c622607951d67394ed05421d6b81966c3693cb0d
Phuc Nguyen <nhphuc@starboardasia.com>
2023-07-06 14:50:44 +0700

    update code

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,15 +5,21 @@
                     <p><a href="/"><img src="{{ asset('assets/images/logo.png') }}"></a></p>
                 </div>
                 <div class="header-top-right">
-                    <p> 
+                    <p>
                         <span id="lbExit"></span>
                         <span id="lbLogin">
-                        <a class="text-animation" target="_blank" href="https://zalo.me/0378304587">Chuyên gia cho kèo</a>
-                        &nbsp;&nbsp;|&nbsp;&nbsp;<a class="text-animation" target="_blank"  href="https://onbet3.com/?inviteCode=8oncom">Đăng ký VIP</a>
-                        &nbsp;&nbsp;|&nbsp;&nbsp;<a  target="_blank"  href="https://8on.com/">XEM LIVE TRỰC TUYẾN</a>
+                          <a  target="_blank"  href="https://8on.com/">XEM LIVE TRỰC TUYẾN</a>
                         </span>
                     </p>
                 </div>
                 <div class="clearfix"></div>
             </div>
+        </div>
+
+        <div class="leftMenu">
+            <ul>
+                <li><a class="text-animation" target="_blank" href="https://zalo.me/0378304587">Chuyên gia cho kèo</a></li>
+                <li><a class="text-animation" target="_blank"  href="https://onbet3.com/?inviteCode=8oncom">Đăng ký VIP</a></li>
+            </ul>
+            
         </div>
